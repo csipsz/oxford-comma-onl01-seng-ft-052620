836324341ff47dc6cf538dddabe9f17b46e432e6
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-27 15:33:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
   last = array.pop
   if array.size > 1 
-  fruits = array.join(", ")
-  "#{fruits}, and #{last}"
+    fruits = array.join(", ")
+    "#{fruits}, and #{last}"
+  else 
+    
+  end 
 end
